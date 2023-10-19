546617c65a89964a693338c205fbaee409923578
jerebondar <gere.bond.ar@gmail.com>
2023-10-19 19:32:30 -0300

    mi primer archivo :D

---
+++ jeje.js
@@ -0,0 +1,21 @@
+let jaja = [ {
+    j: "paaaaaaaaa",
+    p: 3
+},
+{
+    j: "paaaaaaaaa",
+    p: 3
+} ]
+
+let jaeja = [{
+    j: "paaaasaaaaa",
+    p: 33
+},
+{
+    j: "paaaaaaaaa",
+    p: 3
+} ]
+
+let unity = [...jaja, ...jaeja]
+
+console.log(unity)
